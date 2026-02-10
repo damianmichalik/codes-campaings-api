7749854f214ecc1e4c949eb4875abe0a5c0970f8
Damian Michalik <damian.michalik@gmail.com>
2026-02-10 18:35:34 +0100

    Domain tests

--- source/CodesCampaigns.Domain/ValueObjects/Money.cs
+++ source/CodesCampaigns.Domain/ValueObjects/Money.cs
@@ -1,22 +1,22 @@
-﻿namespace CodesCampaigns.Domain.ValueObjects;
+﻿using System.Globalization;
+
+namespace CodesCampaigns.Domain.ValueObjects;
 
 public record Money
 {
     public decimal Amount { get; }
     public CurrencyCode CurrencyCode { get; }
 
-    private Money() => CurrencyCode = null!;
-
     public Money(decimal amount, CurrencyCode currencyCode)
     {
         if (amount < 0)
         {
-            throw new ArgumentException("Amount cannot be negative.", nameof(amount));
+            throw new ArgumentException("Amount cannot be negative");
         }
 
         Amount = amount;
         CurrencyCode = currencyCode ?? throw new ArgumentNullException(nameof(currencyCode));
     }
 
-    public override string ToString() => $"{CurrencyCode} {Amount:N2}";
+    public override string ToString() => $"{CurrencyCode} {Amount.ToString("N2", CultureInfo.InvariantCulture)}";
 }
